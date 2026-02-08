3d16034057de1fa7c273760ee030ba6e397fbd1d
zlatjdals <65696819+zlatjdals@users.noreply.github.com>
2026-02-08 16:59:58 +0900

    Refactor copula_imputation function for simplicity

--- 02_copula_imputation.R
+++ 02_copula_imputation.R
@@ -1,14 +1,8 @@
 # Copula Imputation Function
 
-copula_imputation <- function(data, rep = 10, iter = 1000, burnin = 500, thin = 10, cont_indices = NA) {
-  num_samples <- (iter - burnin) / thin
-  X_sample <- vector("list", rep * num_samples)
-  index <- 1
-  for (i in 1:rep) {
-    result <- gibbs_copula_fully_bayesian(data, iter = iter, burnin = burnin, thin = thin, cont_indices = cont_indices)
-    X_sample[index:(index + num_samples - 1)] <- result$X_sample
-    index <- index + num_samples
-  }
+copula_imputation <- function(data, iter = 1000, burnin = 500, thin = 10, cont_indices = NA) {
+  result <- gibbs_copula_fully_bayesian(data, iter = iter, burnin = burnin, thin = thin, cont_indices = cont_indices)
+  X_sample <- result$X_sample
   X_imputed <- Reduce("+", X_sample) / length(X_sample)
   return(X_imputed)
 }
