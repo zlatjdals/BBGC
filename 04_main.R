f6c1b75e2aa32980d4f7aa92780d41ca420a8289
zlatjdals <65696819+zlatjdals@users.noreply.github.com>
2026-02-08 16:58:58 +0900

    Modify copula_imputation call to include cont_indices

--- 04_main.R
+++ 04_main.R
@@ -14,7 +14,7 @@ data_nonna <- data
 data_mis <- get_MAR_testset(data, prop = sparsity, prop2 = sparsity, miss_force = TRUE)
 index_na <- which(is.na(data_mis))
 
-data_imputed <- copula_imputation(data_mis , rep = 10, iter = 10000, burnin = 5000, thin = 10)
+data_imputed <- copula_imputation(data_mis , iter = 10000, burnin = 5000, thin = 10 , cont_indices = 6:15 )
 
 results <- evaluate_copula(data_imputed, data_nonna, index_na)
 print(results$r_squared)
