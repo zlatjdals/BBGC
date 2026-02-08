52800348b1d6d0249ac0f4747d8f6e8070543674
zlatjdals <65696819+zlatjdals@users.noreply.github.com>
2026-02-08 16:56:45 +0900

    Add cont_indices parameter to copula_imputation function

--- 02_copula_imputation.R
+++ 02_copula_imputation.R
@@ -1,11 +1,11 @@
 # Copula Imputation Function
 
-copula_imputation <- function(data, rep = 10, iter = 1000, burnin = 500, thin = 10) {
+copula_imputation <- function(data, rep = 10, iter = 1000, burnin = 500, thin = 10, cont_indices = NA) {
   num_samples <- (iter - burnin) / thin
   X_sample <- vector("list", rep * num_samples)
   index <- 1
   for (i in 1:rep) {
-    result <- gibbs_copula(data, iter = iter, burnin = burnin, thin = thin, alpha = ncol(data) + 5)
+    result <- gibbs_copula_fully_bayesian(data, iter = iter, burnin = burnin, thin = thin, cont_indices = cont_indices)
     X_sample[index:(index + num_samples - 1)] <- result$X_sample
     index <- index + num_samples
   }
